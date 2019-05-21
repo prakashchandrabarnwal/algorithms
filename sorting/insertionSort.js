781a11e65baade5287ffb5c7d583c3f3450f4924
Prakash Chandra Barnwal <PB00547846@techmahindra.com>
2019-05-21 17:56:04 +0530

    insertion sort

---
+++ sorting/insertionSort.js
@@ -0,0 +1,19 @@
+const insertionSort = (arr) => {
+
+    for(let i = 1 ; i < arr.length; i++){
+        let current = arr[i];
+
+        for(var j=i-1; j>=0; j--){
+                    if(arr[j] < current ){
+                            break;
+                    }
+                    arr[j+1] = arr[j];
+        }
+
+        arr[j+1]=current;
+
+    }
+    return arr;
+}
+
+console.log(insertionSort([2,1,0,76,4]))
